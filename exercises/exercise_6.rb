0e078ba3537707f5974b40bcbff81278654a8ce6
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-10-26 22:13:07 -0700

    exetcise_6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Marina", last_name: "Ivanova", hourly_rate: 50)
+@store1.employees.create(first_name: "Denis", last_name: "Khrustalev", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Polina", last_name: "Khrustaleva", hourly_rate: 65)
+@store2.employees.create(first_name: "Roman", last_name: "Khrustalev", hourly_rate: 55)
+
+@store1.employees.each do |employee|
+  puts "Employee: #{employee.first_name} #{employee.last_name}, Hourly Rate: $#{employee.hourly_rate}"
+end
+@store2.employees.each do |employee|
+  puts "Employee: #{employee.first_name} #{employee.last_name}, Hourly Rate: $#{employee.hourly_rate}"
+end
